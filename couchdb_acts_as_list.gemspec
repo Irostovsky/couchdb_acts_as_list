bb37bb5d710294c2b905afe11a0918f4f9cd6183
Ivan Rostovsky <ir@irostovsky.local>
2012-12-25 14:32:59 +0200

    0.0.1

--- couchdb_acts_as_list.gemspec
+++ couchdb_acts_as_list.gemspec
@@ -6,10 +6,10 @@ require 'couchdb_acts_as_list/version'
 Gem::Specification.new do |gem|
   gem.name          = "couchdb_acts_as_list"
   gem.version       = CouchdbActsAsList::VERSION
-  gem.authors       = ["TODO: Write your name"]
-  gem.email         = ["TODO: Write your email address"]
-  gem.description   = %q{TODO: Write a gem description}
-  gem.summary       = %q{TODO: Write a gem summary}
+  gem.authors       = ["Ivan Rostovsky"]
+  gem.email         = ["ivan.rostovsky@gmail.com"]
+  gem.description   = %q{}
+  gem.summary       = %q{Implementation of the acts as list gem for CouchDB}
   gem.homepage      = ""
 
   gem.files         = `git ls-files`.split($/)
--- lib/couchdb_acts_as_list.rb
+++ lib/couchdb_acts_as_list.rb
@@ -1,5 +1,43 @@
 require "couchdb_acts_as_list/version"
 
 module CouchdbActsAsList
-  # Your code goes here...
+  def self.included(base)
+    base.class_eval do
+      property :position, Integer
+
+      design do
+        view :by_position
+      end
+      before_validation :set_position, on: :create
+      after_destroy :reorder_lower_items
+
+
+      def insert_at pos
+        pos = [[pos.to_i, Locale.count].min, 1].max
+        decrement_positions_on_lower_items self.position
+        self.update_attributes position: nil
+        increment_positions_on_lower_items pos
+        self.update_attributes position: pos
+      end
+
+      private
+      def set_position
+        self.position = Locale.count + 1
+      end
+
+      def reorder_lower_items
+        decrement_positions_on_lower_items self.position
+      end
+
+      def decrement_positions_on_lower_items(pos)
+        self.class.by_position(startkey: pos + 1).each{|l| l.update_attributes position: l.position - 1}
+      end
+
+      def increment_positions_on_lower_items(pos)
+        self.class.by_position(startkey: pos).each{|l| l.update_attributes position: l.position + 1}
+      end
+
+    end
+
+  end
 end
